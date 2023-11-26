7f6436a20c6bae69ba5792cade68e68ddeeb5bab
wolfy1339 <webmaster@wolfy1339.com>
2023-11-25 21:13:09 -0500

    feat: initial version

---
+++ script.js
@@ -0,0 +1,9 @@
+// @ts-check
+
+/**
+ * Replace NodeJS internal module imports with `node:` specifier imports
+ *
+ * @param {import('@octoherd/cli').Octokit} octokit
+ * @param {import('@octoherd/cli').Repository} repository
+ */
+export async function script(octokit, repository) {}
